ebcec9c74d26c7fc02263f8512da99d861de371a
zackees <z@zackees.com>
2023-06-01 00:20:39 -0700

    adds validation to the email forms

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+initSignup(function () {
+    console.log('signup closed');
+});
+
 !function (r, u, m, b, l, e) { r._Rumble = b, r[b] || (r[b] = function () { (r[b]._ = r[b]._ || []).push(arguments); if (r[b]._.length == 1) { l = u.createElement(m), e = u.getElementsByTagName(m)[0], l.async = 1, l.src = "https://rumble.com/embedJS/urq2e0" + (arguments[1].video ? '.' + arguments[1].video : '') + "/?url=" + encodeURIComponent(location.href) + "&args=" + encodeURIComponent(JSON.stringify([].slice.apply(arguments))), e.parentNode.insertBefore(l, e) } }) }(window, document, "script", "Rumble");
 
 Rumble("play", { "video": "v2nwlhq", "div": "rumble_v2nwlhq" });
@@ -33,6 +37,64 @@ let jobId = setInterval(() => {
 }, 8);
 
 
-initSignup(function () {
-    console.log('signup closed');
+
+function dialogMessage(message) {
+    let dialog = document.getElementById('dialogBox');
+    let dialogMessage = document.getElementById('dialogBoxmMessage');
+    dialogMessage.innerHTML = message;
+    dialog.showModal();
+}
+
+function validateEmail(email) {
+    const regex = '[^\\.\\s@:](?:[^\\s@:]*[^\\s@:\\.])?@[^\\.\\s@]+(?:\\.[^\\.\\s@]+)*';
+    function emailRegex({ exact } = {}) {
+        return exact ? new RegExp(`^${regex}$`) : new RegExp(regex, 'g');
+    }
+    const isEmail = emailRegex({ exact: true }).test(email);
+    return isEmail;
+}
+
+function validateName(name) {
+    let re = /^[a-zA-Z\s]+$/;
+    return re.test(name);
+}
+
+document.getElementById('btn-signup').addEventListener('click', function (event) {
+    let emailInput = document.getElementById('signup_email').value;
+    if (!validateEmail(emailInput)) {
+        dialogMessage("Invalid email, please try again.")
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+    }
+    if (!validateName(document.getElementById('singup_first_name').value)) {
+        dialogMessage("Invalid name, please try again.")
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+    }
+    let form = document.getElementById('user_form');
+    const $firstName = document.getElementById('singup_first_name');
+    const $email = document.getElementById('signup_email');
+    signupAddContact($firstName.value, $email.value);
+    event.preventDefault();
+    event.stopPropagation();
+    /*
+    fetch('http://localhost:8080/add_user_contact', {  // specify your URL here
+        method: 'POST',    // or 'GET', depends on your requirements
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch((error) => console.error('Error:', error));
+    */
+});
+
+document.getElementById('closeBtn').addEventListener('click', function (event) {
+    let dialog = document.getElementById('dialogBox');
+    dialog.close();
 });
--- signup.js
+++ signup.js
@@ -1,53 +1,38 @@
 
-let callbackDismissed = null
+let _signupCallbackDismissed = null
+let $signupCloseBtn = null
+let $signup = null
 
 function signupIsActive() {
-    const $signup = document.querySelector('#signup');
     return $signup.classList.contains('active')
 }
 
+function signupAddContact(name, email) {
+    let message = `Hi ${name},\n\nI'm writing to let you know that I've added your email ${email} to my contact list.`
+    alert(message)
+}
+
+function signupDismissed() {
+    $signup.classList.remove('active')
+    $signupCloseBtn.removeEventListener('click', arguments.callee)
+    // Remove element from dom after a timeout.
+    setTimeout(() => {
+        let $signup = document.querySelector('#signup')
+        $signup.style.display = 'none'
+    }, 1000)
+    _signupCallbackDismissed()
+}
+
 function initSignup(cbDismissed) {
-    callbackDismissed = cbDismissed
-    const $signup = document.querySelector('#signup')
+    _signupCallbackDismissed = cbDismissed
+    $signup = document.querySelector('#signup')
+    $signupCloseBtn = document.querySelector('#signup-close-btn')
+    //assert($signupCloseBtn, 'signupCloseBtn not found')
+    //assert($signup, 'signup not found')
     $signup.classList.add('active')
-    /*
-    if (!signupFrag) {
-      globalThis.alert('signup html not found')
-    }
-    const $target = document.getElementById('signup')
-    // Generate a div and populate it with the string from the fragment.
-    const $div = document.createElement('div')
-    $div.innerHTML = signupFrag
-    // Now get the signup html
-    const $signup = $div.querySelector('#signup')
-    // Now replace the target with the fragment.
-    $target.replaceWith($signup)
-    */
-    let signupCloseBtn = document.querySelector('#signup-close-btn')
-    signupCloseBtn.addEventListener('click', (e) => {
-        //debugger
-        /*
-        if (signupIsActive()) {
-          // does the click target the signup form?
-          if (e.target.closest('#signup')) {
-            // yes, do nothing
-            return
-          }
-        }
-        */
-        $signup.classList.remove('active')
+    $signupCloseBtn.addEventListener('click', (e) => {
         e.preventDefault()
         e.stopPropagation()
-        // remove click handler
-        signupCloseBtn.removeEventListener('click', arguments.callee)
-        setTimeout(() => {
-            let $signup = document.querySelector('#signup')
-            $signup.style.display = 'none'
-        }, 1000)
-        callbackDismissed()
-        //if (window.location.hash === '#signup') {
-        //  window.location.hash = ''
-        //}
-        // e.preventDefault()
+        signupDismissed()
     })
 }
