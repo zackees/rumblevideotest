405a70024c29a5a7fa130c429f86d19a79d55bf9
zackees <z@zackees.com>
2023-06-01 22:36:01 -0700

    adds signup=true

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ HERE = os.path.dirname(__file__)
 os.chdir(HERE)
 
 PORT = 7000
-webbrowser.open(f'http://localhost:{PORT}')
+webbrowser.open(f'http://localhost:{PORT}?signup=true')
 os.system(f'python -m http.server {PORT}')
