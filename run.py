92954071fb749bee7f04462136f9f91e7845fe93
zackees <zachvorhies@protonmail.com>
2023-06-11 16:33:59 -0400

    update run to use python exe from sys

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 import webbrowser
 import os
+import sys
 
 HERE = os.path.dirname(__file__)
 os.chdir(HERE)
 
+PYTHON_EXE = sys.executable
+
 PORT = 7002
 webbrowser.open(f'http://localhost:{PORT}/mainpage.html')
 try:
-    os.system(f'python -m http.server {PORT}')
+    os.system(f'"{PYTHON_EXE}" -m http.server {PORT}')
 except KeyboardInterrupt:
     pass
